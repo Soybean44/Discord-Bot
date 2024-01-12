7b2550c9a20faebcc798ad291613d25f1937d4ce
Sovereign Shahid <sovereignshahid@gmail.com>
2024-01-11 20:59:31 -0700

    changing session code

--- db.py
+++ db.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column
 
 engine = create_engine("sqlite+pysqlite:///database.db")
 
+session = Session(engine)
+
 
 class Base(DeclarativeBase):
     pass
@@ -21,24 +23,22 @@ Base.metadata.create_all(engine)
 
 
 def uploadConfession(confession, timestamp, user):
-    global engine
+    global engine, session
     confession_obj = Confession(
         id=None, confession=confession, timestamp=timestamp, user=user
     )
-    with Session(engine) as session:
-        session.add(confession_obj)
-        session.commit()
+    session.add(confession_obj)
+    session.commit()
 
     return confession_obj
 
 
 def getConfession(id):
-    global engine
+    global engine, session
     stmt = select(Confession).where(Confession.id == id)
     confessions = []
-    with Session(engine) as session:
-        for row in session.exec(stmt):
-            confessions.append(row)
+    for row in session.exec(stmt):
+        confessions.append(row)
     if len(confessions) > 0:
         return confessions[0]
     else:
